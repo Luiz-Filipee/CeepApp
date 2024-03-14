efaf1270d22578b469956d2462e1349148d1635e
luizfilipe <luizkato8@Gmail.com>
2024-03-14 14:13:56 -0400

    fix: implementando as nova constantes que referenciam o titulo do formulario, eidata ou insere

--- app/src/main/java/br/luizfilipe/ceepapp/ui/activity/Constantes.java
+++ app/src/main/java/br/luizfilipe/ceepapp/ui/activity/Constantes.java
@@ -4,8 +4,8 @@ public interface Constantes {
     String CHAVE_NOTA = "nota";
     int CODIGO_REQUISICAO_INSERE_NOTA = 1;
     String TITULO_APPBAR_LISTA_NOTAS_ACTIVITY = "Notas";
-    String TITULO_APPBAR_FORMULARIO_NOTAS_ACTIVITY = "Insere Nota";
-
+    String TITULO_APPBAR_INSERE = "Insere Nota";
+    String TITULO_APPBAR_ALTERA_NOTA = "Altera nota";
     String CHAVE_POSICAO = "posicao";
     int CODIGO_REQUISICAO_ALTERA_NOTA = 2;
     int POSICAO_INVALIDA = -1;
